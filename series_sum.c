13eb57d71a1e9a58e79137340e63d28861a8470d
mariafayaz773-prog <mariafayaz773@gmail.com>
2025-10-09 21:22:01 +0530

    Update series_sum.c

--- series_sum.c
+++ series_sum.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-
 int main() {
     int n, i;
+    float sum = 0;
     printf("Enter the number of terms: ");
     scanf("%d", &n);
-    for(i = 1; i <= n; i++)
+    for(i = 1; i <= n; i++) 
     {
-        printf("1/%d ", 2 * i);
-    }
-    printf("\n");
+        sum = sum + 1.0 / (2 * i); 
+    }   
+    printf("\nTotal sum = %f\n", sum);
     return 0;
 }
